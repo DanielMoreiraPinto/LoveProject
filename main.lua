5efdaa8602960f93a925b3879c0f1fb834b55050
Daniel Moreira Pinto <danielmoreirapinto.ti@gmail.com>
2019-06-13 11:51:56 -0300

    Track and Field with rectangles without gameover complete

--- main.lua
+++ main.lua
@@ -1,6 +1,14 @@
 require("colisao")
 
 function love.load()
+	velocidade = 20
+
+	chao = {}
+	chao.x = 0
+	chao.y = 450
+	chao.largura = 1000
+	chao.altura = 50
+
 	jogador = {}
 	jogador.x = 30
 	jogador.y = 400
@@ -10,14 +18,17 @@ function love.load()
 	jogador.pulo = 800
 	jogador.gravidade = -40
 
-	chao = {}
-	chao.x = 0
-	chao.y = 450
-	chao.largura = 1000
-	chao.altura = 50
+	inimigos = {}
+	inimigo_x = 1000
+	inimigo_y = 400
+	inimigo_largura = 30
+	inimigo_altura = 50
+
+	intervaloInimigo = 200
 end
 
 function love.update(dt)
+	--Atualização do jogador
 	jogador.y = jogador.y + jogador.velocidadeY * dt
 
 	jogador.velocidadeY = jogador.velocidadeY - jogador.gravidade
@@ -30,6 +41,22 @@ function love.update(dt)
 	if love.keyboard.isDown("space") and jogador.velocidadeY == 0 and (jogador.y + jogador.altura) >= chao.y then
 		jogador.velocidadeY = -jogador.pulo
 	end
+
+	--Criação e atualização dos inimigos, controle da frequência dos inimigos
+	intervaloInimigo = intervaloInimigo - 1.5
+	
+	if intervaloInimigo <= 0 then
+		intervaloInimigo = love.math.random(60, 255)
+		novoInimigo = {x = inimigo_x, y = inimigo_y}
+		table.insert(inimigos, novoInimigo)
+	end
+
+	for i, inimigo in ipairs(inimigos) do
+		inimigo.x = inimigo.x - velocidade
+		if inimigo.x + inimigo_largura < 0 then
+			table.remove(inimigos, i)
+		end
+	end
 end
 
 function love.draw()
@@ -39,5 +66,8 @@ function love.draw()
 	love.graphics.setColor(1, 0, 0)
 	love.graphics.rectangle("fill", jogador.x, jogador.y, jogador.largura, jogador.altura)
 
-	love.graphics.print(jogador.velocidadeY, 20, 20)
+	for i, inimigo in ipairs(inimigos) do
+		love.graphics.setColor(0, 0, 1)
+		love.graphics.rectangle("fill", inimigo.x, inimigo.y, inimigo_largura, inimigo_altura)
+	end
 end
