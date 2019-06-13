b9f2d83102536dd493e95931257b0c1bd9178754
Daniel Moreira Pinto <danielmoreirapinto.ti@gmail.com>
2019-06-13 09:43:02 -0300

    Commit for safeguard of the changes because my laptop is the worst

--- main.lua
+++ main.lua
@@ -1,9 +1,15 @@
 function love.load()
 	jogador = {}
-	jogador.x = 20
-	jogador.y = 20
+	jogador.x = 30
+	jogador.y = 400
 	jogador.largura = 70
 	jogador.altura = 50
+
+	chao = {}
+	chao.x = 0
+	chao.y = 450
+	chao.largura = 1000
+	chao.altura = 50
 end
 
 function love.update()
@@ -11,5 +17,7 @@ function love.update()
 end
 
 function love.draw()
+	love.graphics.rectangle("fill", chao.x, chao.y, chao.largura, chao.altura)
+
 	love.graphics.rectangle("fill", jogador.x, jogador.y, jogador.largura, jogador.altura)
 end
